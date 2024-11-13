e09e28e9121b1cfe6452e79f52d4de8e63b7f50d
joaoezx <joao.a.freitas0405@gmail.com>
2024-11-13 07:03:26 -0300

    bank account

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [BankAccountsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{ts,.js}'],
+      synchronize: true,
+    }),
+    BankAccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/bank-accounts/entities/bank-account.entity.ts
+++ src/bank-accounts/entities/bank-account.entity.ts
@@ -1,9 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class BankAccount {
+  @PrimaryGeneratedColumn()
   user_id: string;
+
+  @Column()
   account_name: string;
+
+  @Column()
   account_type: string;
+
+  @Column()
   account_description: string;
+
+  @Column()
   initial_ammount: number;
+
+  @Column()
   created_at: string;
+
+  @Column()
   updated_at: string;
 }
